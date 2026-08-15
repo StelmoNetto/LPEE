594719fad8164c0e9d6dcf38ebd95166c670760d
Usuário <user@example.com>
2026-08-15 20:38:48 -0300

    Uso e manipulação de caracteres (char), tabela ASCII e caracteres especiais.

---
+++ parte_1/introducao/declaracao/aprofundando_tipo_caractere.c
@@ -0,0 +1,9 @@
+﻿int main()
+{
+	char c = 'A'; // O código ASCII 65 do caractere 'A' é armazenado na variável c
+	c = 'a'; // O código ASCII 97 do caractere 'a' é armazenado na variável c
+	c = 65; // O código ASCII 65 do caractere 'A' é armazenado na variável c
+
+	char letraNegativa = -2; /* Equivalente a 254 sem sinal e corresponde um caractere especial (quadrado, linha ou a letra ■) ASCII estendido, dependendo da codificação utilizada no terminal */
+	return 0;
+}
