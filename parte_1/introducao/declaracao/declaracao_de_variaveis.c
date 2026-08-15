642222810791617d1203f071a92f829f00c13b00
Usuário <user@example.com>
2026-08-15 20:38:45 -0300

    Exemplos fundamentais de declaração, inicialização e atribuição de variáveis de tipos primitivos.

---
+++ parte_1/introducao/declaracao/declaracao_de_variaveis.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+
+int main()
+{
+    _Bool desligado;
+    bool ligado = true; //Incluir biblioteca stdbool.h
+    char caractere = 'b';
+    int numeroDeAlunos = 4;
+    float velocidadeEmMporS = 9.85f;
+	const double PI = 3.141592653589793; //Vari�vel constante. N�o pode ser alterada
+	long double numeroDeAlunosNoMundo = 7.9e9; //7.9 bilh�es de alunos no mundo
+	char sexo[] = "masculino"; //10 caracteres = 9 letras + 1 caractere nulo '\0'
+
+    //Linguagem C � caso sensitivo:
+    int a, A, maior, Maior, mAior, maIor; //S�o todas diferentes
+
+    return 0;
+}
