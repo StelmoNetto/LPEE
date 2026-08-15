2156601fe23135d658d8e8a2ed64b7a3c73a1013
Usuário <user@example.com>
2026-08-15 20:39:12 -0300

    Uso de sequências de escape para formatação de texto.

---
+++ parte_1/introducao/saida/caracteres_especiais_de_saida.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    //printf("Item:\t\"Livro\" \\ 100%%\nErro!\b\a\rSucesso");
+    //Faça o passo a passo
+    printf("Item:\t");
+    printf("\"Livro\"");
+    printf("\\ ");
+    printf("100%%\n");
+    printf("Erro!");
+    printf("\b");
+    printf("\a");
+    printf("\r");
+    printf("Sucesso");
+    return 0;
+}
