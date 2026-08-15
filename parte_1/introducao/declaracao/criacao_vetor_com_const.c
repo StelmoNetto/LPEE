1803c315429b26251c4d770e89c1e5eac96cb301
Usuário <user@example.com>
2026-08-15 20:39:00 -0300

    Declaração de vetores com tamanhos fixos utilizando o qualificador const.

---
+++ parte_1/introducao/declaracao/criacao_vetor_com_const.c
@@ -0,0 +1,13 @@
+int main()
+{
+	const int TAMANHO = 5;
+	
+	//int vetor[TAMANHO]; //TAMANHO não é uma constante em tempo de compilação
+
+	enum {
+		LARGURA_TELA = 800,
+		ALTURA_TELA = 600
+	};
+
+	int tela[LARGURA_TELA * ALTURA_TELA];
+}
