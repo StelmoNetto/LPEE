b552cce7e35cbb012997f91371a9c297707a8064
Usuário <user@example.com>
2026-08-15 20:40:35 -0300

    Cálculo de potenciação e raízes com pow(), sqrt(), cbrt() e hypot().

---
+++ parte_1/introducao/funcoes_matematicas/operacoes_potenciacao_e_raizes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("pow(2,3)   = %.0f\n", pow(2, 3));
+    printf("sqrt(25)   = %.0f\n", sqrt(25));
+    printf("cbrt(27)   = %.0f\n", cbrt(27));
+    printf("hypot(3,4) = %.0f\n", hypot(3, 4));
+
+    return 0;
+}
