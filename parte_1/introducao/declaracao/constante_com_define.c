bd930443173aa310cb49b28f2a9d249b80acf064
Usuário <user@example.com>
2026-08-15 20:38:52 -0300

    Definição de constantes com a diretiva de pré-processador #define.

---
+++ parte_1/introducao/declaracao/constante_com_define.c
@@ -0,0 +1,19 @@
+#define VALOR_INICIAL 10
+
+int main() 
+{
+	int valor = VALOR_INICIAL;
+	
+	return 0;
+}
+
+/*
+* Pré-processador: O código define uma constante chamada VALOR_INICIAL com o valor 10 usando a diretiva #define. Isso significa que, em qualquer lugar do código onde VALOR_INICIAL for usado, ele será substituído pelo valor 10 durante a fase de pré-processamento.
+
+int main() 
+{
+	int valor = 10;
+
+	return 0;
+}
+*/
