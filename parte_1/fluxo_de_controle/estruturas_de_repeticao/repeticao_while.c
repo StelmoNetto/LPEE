a6e54ecc0b77003d99a9c5a1c4d1fc16ece74b20
Usuário <user@example.com>
2026-08-19 09:50:00 -0300

    Uso da estrutura de repetição com teste no início (`while`).

---
+++ parte_1/fluxo_de_controle/estruturas_de_repeticao/repeticao_while.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int contador = 0; // Inicialização
+
+    while (contador < 3) // Condição do Loop
+    {  // Corpo de Loop
+        printf("%d\n", contador);
+        contador++; // Expressão de atualização
+    }
+    return 0;
+}
