42906985a733e600988e5c3bc327050b8a21c9bd
Usuário <user@example.com>
2026-08-19 09:50:02 -0300

    Uso da estrutura de repetição com controle de contador (`for`).

---
+++ parte_1/fluxo_de_controle/estruturas_de_repeticao/repeticao_for.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int contador = 0; contador < 3; contador++)
+        printf("%d\n", contador); // Corpo de loop
+
+    for (contador = 0; contador < 3; contador++)
+    {
+        printf("%d\n", contador); // Corpo de loop
+    }
+    return 0;
+}
